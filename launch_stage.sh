5395f591d20a06280e4d00636b206f33b17ed462
Ian Wallace <ian.southd@gmail.com>
2015-08-24 23:15:45 -0700

    effects

---
+++ launch_stage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OUTPUT=`./extract_device.sh "Built-in Output"`
+INPUT=`./extract_device.sh "Built-in Input"`
+LAUNCHPAD=`./extract_midi.sh "Launchpad"`
+
+killall chuck
+sleep 1
+chuck --loop --dac:$OUTPUT --adc:$INPUT &
+sleep 1
+chuck + lick-import.ck
+chuck + effects/drone_stack.ck
